912aa5c6baf34808ded01fcf30ff89512de442e4
muhti46 <muhteremdemir@gmail.com>
2025-02-18 11:20:08 +0100

    I created symund project

---
+++ src/test/java/com/symund/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.symund.pages;
+import com.symund.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void clickMenuByText(String text){
+     WebElement menuElement= Driver.getDriver().findElement(By.xpath("(//li[@data-id='"+text.toLowerCase()+"'])[1]"));
+     menuElement.click();
+    }
+    }
+
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,15 @@
+@login
+Feature: Users should be able to login
+
+  Background: User is already in the log in page
+    Given the user is on the login page
+
+
+  Scenario: Verify login with different user types
+    #Given the user logged in as "<userType>"
+    When the user logged in with username as "Employee62" and password as "Employee123"
+    Then the user clicks "file" menu
+
+
+
+
