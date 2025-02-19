412b44149f83f8fa272b6177aec4ddf374c9457d
muhti46 <muhteremdemir@gmail.com>
2025-02-19 16:13:38 +0100

    added method definition

--- src/test/java/com/symund/pages/BasePage.java
+++ src/test/java/com/symund/pages/BasePage.java
@@ -2,6 +2,7 @@ package com.symund.pages;
 import com.symund.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
@@ -10,10 +11,23 @@ public abstract class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(xpath = "//div[@class='logo logo-icon']")
+    public  WebElement logoIcon;
+
+
+    /**This method click menu elements into navigation menu
+     * @param text
+     */
     public void clickMenuByText(String text){
      WebElement menuElement= Driver.getDriver().findElement
              (By.xpath("(//li[@data-id='"+text.toLowerCase()+"'])[1]"));
      menuElement.click();
     }
+    public void logoClick(){
+    logoIcon.click();
+    }
+
+
+
     }
 
