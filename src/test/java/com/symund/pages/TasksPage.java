7952b5b983467399d6a072bf5c89078b5497068e
muhti46 <muhteremdemir@gmail.com>
2025-02-21 22:10:34 +0100

    some code redundancies are removed.

--- src/test/java/com/symund/pages/TasksPage.java
+++ src/test/java/com/symund/pages/TasksPage.java
@@ -35,39 +35,6 @@ public class TasksPage extends BasePage {
     public WebElement addingTaskBox;
 
     /**
-     * {@code tasksList} List of WebElements representing all task titles displayed in the currently selected task list.
-     * Each WebElement in this list corresponds to a task title.
-     */
-    @FindBy(xpath = "//div[@class='title']/span")
-    public List<WebElement> tasksList;
-
-    /**
-     * {@code completedTasks} List of WebElements representing the titles of tasks that are marked as completed.
-     * These tasks are typically displayed under a "Completed" section or with a different visual style.
-     */
-    @FindBy(xpath = "//ol[contains(@class, 'completed')]//div[@class='title']")
-    public List<WebElement> completedTasks;
-
-    /**
-     * {@code completedTaskCheckbox} List of WebElements representing checkboxes of tasks that are marked as completed.
-     * This might be used to interact with checkboxes specifically within the completed tasks section,
-     * although the current XPath is incomplete and likely incorrect.
-     * **Note:** The current XPath `//li[contains(@class, 'task-item') and
-     * contains(@class, 'done')]//span[normalize-space(text())='']/../../preceding-sibling::div/label`
-     * is likely to have issues as it's looking for a task name with empty text `''`. This XPath might need to be reviewed
-     * and corrected based on the actual HTML structure to find checkboxes of completed tasks effectively.
-     */
-    @FindBy(xpath = "//li[contains(@class, 'task-item') and contains(@class, 'done')]//span[normalize-space(text())='']/../../preceding-sibling::div/label")
-    public List<WebElement> completedTaskCheckbox;
-
-    /**
-     * {@code completedButton} WebElement represents the "Completed" navigation button in the sidebar.
-     * Clicking this button usually navigates the user to the "Completed" tasks view.
-     */
-    @FindBy(xpath = "//li[@id='collection_completed']")
-    public WebElement completedButton;
-
-    /**
      * Verifies if a newly created task list name is correctly displayed in the sidebar.
      * It finds the created task list element by its title and asserts if the displayed name matches the expected name.
      *
@@ -129,16 +96,4 @@ public class TasksPage extends BasePage {
         Assert.assertTrue("Task '" + taskName + "' was not found in the '" + listName + "' list", taskFound);
     }
 
-    /**
-     * Retrieves the checkbox WebElement for a given task name.
-     * This method finds the checkbox element that is associated with a task name.
-     *
-     * @param taskName The name of the task whose checkbox is to be retrieved.
-     * @return WebElement representing the checkbox of the specified task.
-     */
-    public WebElement getCheckboxForTask(String taskName) {
-        return Driver.getDriver().findElement(
-                By.xpath("//span[.='"+taskName+"']/../../preceding-sibling::div/label")
-        );
-    }
 }
