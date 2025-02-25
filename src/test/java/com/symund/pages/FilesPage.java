82dd7f6b32121866f61bae2e34a1873bb3b8c02d
Andrada <andradaciuclea@yahoo.com>
2025-02-25 21:13:13 +0100

    just a small fix

--- src/test/java/com/symund/pages/FilesPage.java
+++ src/test/java/com/symund/pages/FilesPage.java
@@ -12,13 +12,23 @@ import java.util.List;
 
 public class FilesPage extends BasePage {
 
-    public FilesPage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+    @FindBy(xpath = "//label[@for='select_all_files']")
+    public WebElement selectAllCheckbox;
+
+    @FindBy(xpath = "//input[contains(@id,'select-files')]")
+    public List<WebElement> allCheckBoxes;
+
+    @FindBy(xpath = "(//div[@id='headerName-container']//span)[1]")
+    public WebElement totalNumberOfFoldersFiles;
+
+    @FindBy(xpath = "//tr[@data-type='file']")
+    public List<WebElement> files;
+
+    @FindBy(xpath = "//tr[@data-type='dir']")
+    public List<WebElement> folders;
 
     @FindBy(xpath = "//a[@class='button new']")
     public WebElement addButton;
-
 
     @FindBy(xpath = "//input[@type='file']")
     public WebElement uploadFile;
